1c643c7ae6680424b4b5637756407d30119c342c
atomius <atomius1@gmail.com>
2016-11-29 18:14:07 +0100

    fixed error in test_image.lua, renamed function love.run to love.update. (love.run was the wrong function here)

--- _tests/test_image.lua
+++ _tests/test_image.lua
@@ -1,18 +1,27 @@
 
 require "image"
 
+
 tiles_img = nil
 
 function love.load()
+	
 	autoMidHandle(true)
 	tiles_img = loadAnimImage("media/tiles.png", 32, 20, 1, 5)
 	assert(tiles_img)
 end
 
 
-function love.run()
+function love.update()
+	--[[
+	assert(tiles_img)
+	assert(tiles_img.img)
+	print("ok")
+	--]]
 	for i = 1, 5 do
+		--TODO: this doesn't draw anything... why?
 		drawImage(tiles_img, 100, 50 + 32 * (i-1))
+		print(tiles_img, 100, 50 + 32 * (i-1))
 	end
 end
 
