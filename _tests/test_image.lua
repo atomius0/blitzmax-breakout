d9e2a4e7b944eac8fa6e6fedfc1bd1e3f4eabcff
atomius <atomius1@gmail.com>
2016-11-29 18:37:53 +0100

    fixed bug that caused the program not to draw anything

--- _tests/test_image.lua
+++ _tests/test_image.lua
@@ -11,15 +11,17 @@ function love.load()
 	assert(tiles_img)
 end
 
-
-function love.update()
+-- you can't draw in love.update,
+-- because the mainloop clears the screen between love.update and love.draw!
+--function love.update()
+function love.draw()
 	--[[
 	assert(tiles_img)
 	assert(tiles_img.img)
 	print("ok")
 	--]]
 	for i = 1, 5 do
-		--TODO: this doesn't draw anything... why?
+		--TODO: this draws the same tile 5 times (the green one)
 		drawImage(tiles_img, 100, 50 + 32 * (i-1))
 		print(tiles_img, 100, 50 + 32 * (i-1))
 	end
