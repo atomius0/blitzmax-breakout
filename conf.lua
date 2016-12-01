691b92366aa834c40bc1e7f2c5715cf691a2a97e
atomius <atomius1@gmail.com>
2016-12-01 17:58:21 +0100

    added conf.lua

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+	t.version = "0.10.2"
+	t.window.title = "Breakout"
+	t.window.width = 640
+	t.window.height = 480
+	t.window.vsync = true
+	--t.window.fullscreen = true
+	t.window.fullscreentype = "desktop"
+end
