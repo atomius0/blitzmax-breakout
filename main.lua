atomius0/blitzmax-breakout

commit c73f8a02b53d2455a269f5bb4db42afd588ec3a2

implemented display scaling (integer scaling) made the window resizable

main.lua
@@ -61,6 +61,9 @@ maxf = math.max
 function sin(n) return math.sin(math.rad(n)) end
 function cos(n) return math.cos(math.rad(n)) end
 
+-- current display/window resolution, for resolution scaling
+winres = {w = 0, h = 0, scale = 1}
+
 
 do Ball = createclass()
 	Ball.x, Ball.y = 0, 0
@@ -218,6 +221,7 @@ end --EndType
 function love.load()
 	-- width and height are set in conf.lua
 	WIDTH, HEIGHT = love.graphics.getDimensions() --Graphics WIDTH,HEIGHT,DEPTH
+	winres.w, winres.h = WIDTH, HEIGHT
 	
 	autoMidHandle(true) --AutoMidHandle True
 	
@@ -264,6 +268,7 @@ end
 
 
 function love.draw()
+	setScale()
 	-- Draw Level
 	drawLevel()-- DrawLevel()
 
@@ -285,6 +290,25 @@ function love.draw()
 	--love.graphics.print("FPS: " .. tostring(love.timer.getFPS()), 400, 5)
 	
 	-- Flip
+end
+
+
+function love.resize(w, h)
+	-- use one scale value only, to keep aspect ratio
+	
+	local scale = math.min(w / WIDTH, h / HEIGHT)
+	
+	scale = math.floor(scale) -- integer scale
+	
+	winres.w = w
+	winres.h = h
+	winres.scale = scale
+end
+
+
+function setScale()
+	love.graphics.origin() -- why is this needed? love.run is supposed to call this before love.draw
+	love.graphics.scale(winres.scale, winres.scale)
 end
 
 

conf.lua
@@ -6,4 +6,5 @@ function love.conf(t)
 	t.window.vsync = true
 	--t.window.fullscreen = true
 	t.window.fullscreentype = "desktop"
+	t.window.resizable = true
 end

commit 4a387a1350dcbd9f7befcefc1d399669c85f67fe

removed unnecessary call to love.graphics.origin() in function setScreenScale
The call was only required because of the bug that was introduced in commit c73f8a02b5 and fixed in commit 36e761a63a
(2 functions with the same name "setScale")

main.lua
@@ -316,7 +316,9 @@ end
 
 
 function setScreenScale()
-	love.graphics.origin() -- why is this needed? love.run is supposed to call this before love.draw
+	-- the call to love.grapics.origin is not needed anymore. It was only required because of the
+	-- scaling bug that was introduced in commit c73f8a02b5 and fixed in commit 36e761a63a
+	--love.graphics.origin() -- why is this needed? love.run is supposed to call this before love.draw
 	
 	-- we have to translate first, then scale. otherwise the translate-offsets will be scaled, too!
 	love.graphics.translate(winres.offs_x, winres.offs_y)